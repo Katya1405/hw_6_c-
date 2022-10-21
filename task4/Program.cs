320faf617977ce1d8c40be25aa1824eba05d1292
Katya <bozhkova-ekaterina@mail.ru>
2022-10-21 17:29:00 +0300

    task4 is solved

---
+++ task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу копирования массива
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+void CopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+    Console.WriteLine("Копия представленного массива: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        copyArray[i] = array[i];
+        Console.Write($"{copyArray[i]} ");
+    }
+}
+
+int[] array = new int[5];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+CopyArray(array);
