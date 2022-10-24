6c77892a33eb5714f8c10f30e25ad3695d9e8507
Katya <bozhkova-ekaterina@mail.ru>
2022-10-24 11:30:46 +0300

    finishh

--- task4/Program.cs
+++ task4/Program.cs
@@ -15,17 +15,6 @@ void PrintArray(int[] array)
 void CopyArray(int[] array)
 {
     int[] copyArray = new int[array.Length];
-    Console.WriteLine("Копия представленного массива: ");
     for (int i = 0; i < array.Length; i++)
-    {
         copyArray[i] = array[i];
-        Console.Write($"{copyArray[i]} ");
-    }
 }
-
-int[] array = new int[5];
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-CopyArray(array);
